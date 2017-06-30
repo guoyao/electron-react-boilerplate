f39bbaa3049b5e84ab5a7c2c8cfca1966b66810c
wuguoyao <wuguoyao@baidu.com>
2017-06-30 23:04:56 +0800

    调整代码风格

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,20 +1,20 @@
 // @flow
 import React from 'react';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'react-router-redux';
 import Routes from '../routes';
 
 type RootType = {
-  store: {},
-  history: {}
+    store: {},
+    history: {}
 };
 
-export default function Root({ store, history }: RootType) {
-  return (
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Routes />
-      </ConnectedRouter>
-    </Provider>
-  );
+export default function Root({store, history}: RootType) {
+    return (
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <Routes />
+            </ConnectedRouter>
+        </Provider>
+    );
 }
